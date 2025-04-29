439aed8c8c3bef298728220ff350f8d8309c8964
George Stephanis <daljo628@gmail.com>
2025-04-29 10:45:07 -0400

    Null updateurl, set attribution.

--- mega-menu-block.php
+++ mega-menu-block.php
@@ -5,10 +5,11 @@
  * Requires at least: 6.5
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            Nick Diego
+ * Author:            BeThink Studio, based on prior work by Nick Diego
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       mega-menu-block
+ * Update URI:        false
  *
  * @package           mega-menu-block
  */
