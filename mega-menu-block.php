168f75e921a08c9cd9627e04ada15ddab15f7f0a
George Stephanis <daljo628@gmail.com>
2025-05-07 11:13:21 -0400

    Tidy capitalization.

--- mega-menu-block.php
+++ mega-menu-block.php
@@ -5,7 +5,7 @@
  * Requires at least: 6.5
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            BeThink Studio, based on prior work by Nick Diego
+ * Author:            Bethink Studio, based on prior work by Nick Diego
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       mega-menu-block
